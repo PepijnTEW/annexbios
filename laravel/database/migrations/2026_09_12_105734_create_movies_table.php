6c10b653d9df9f20738a29817b70f2cc04e37c6c
PepijnTEW <pepijntewinkel@gmail.com>
2026-09-12 14:40:22 +0200

    added movies migration

---
+++ laravel/database/migrations/2026_09_12_105734_create_movies_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('movies', function (Blueprint $table) {
+            $table->id('movie_id');
+            $table->text('title');
+            $table->text('description');
+            $table->date('release_date');
+            $table->text('language')->default('en');
+            $table->decimal('imd_rating', 4, 3)->nullable();
+            $table->text('poster_path')->nullable();
+            $table->boolean('active')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('movies');
+    }
+};
