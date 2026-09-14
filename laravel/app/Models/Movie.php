75688dc3807e572a7be7a74e252eb6747ce148e9
Thomas Verzijl <thomasverzijl27@gmail.com>
2026-09-14 13:44:44 +0200

    Revert "Merge branch 'dev' of https://github.com/PepijnTEW/annexbios into SearchPage"
    This reverts commit d31805e0e05a6bafa905923e2db99fcba3f16d4d, reversing
    changes made to 14359e798e11d4b6c3458326a39527ff83a0435b.

--- laravel/app/Models/Movie.php
+++ laravel/app/Models/Movie.php
@@ -2,32 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Movie extends Model
 {
-    use HasFactory;
-    //Tells to laravel what table it should use
-    protected $table = 'movies';
-
-    //Sets primary key
-    protected $primaryKey = 'movie_id';
-    public $incrementing = true;
-
-    protected $fillable = [
-        'title',
-        'description',
-        'release_date',
-        'language',
-        'imdb_rating',
-        'poster_path',
-        'active',
-    ];
-
-    protected $casts = [
-        'release_date' => 'date',
-        'imdb_rating' => 'float',
-        'active' => 'boolean',
-    ];
+    //
 }
