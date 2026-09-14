8514bb252b7fca44a7054c065e69598edbe6137c
Marylou Groeneveld <marylougroeneveld@gmail.com>
2026-09-14 20:24:26 +0200

    add commented routes, waiting for migrations

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -8,6 +8,10 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
+//still need to add the middleware!! don't have the tokens set up yet! 'middleware' => 'auth:sanctum'
 Route::group(['prefix' => 'v1', 'namespace' => 'App\Http\Controllers\Api\V1'], function () {
     Route::apiResource('movies', MovieController::class); // api/v1/movies
+    // Route::apiResource('showtimes', ShowtimeController::class); // api/v1/showtimes
+
+    // Route::apiResource('cinemas', CinemaController::class); // api/v1/cinemas
 });
