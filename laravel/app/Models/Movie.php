25ea06637e16150fd10e26fbc61c4749d6ddd298
PepijnTEW <pepijntewinkel@gmail.com>
2026-09-14 11:06:18 +0200

    added movie model

--- laravel/app/Models/Movie.php
+++ laravel/app/Models/Movie.php
@@ -6,5 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class Movie extends Model
 {
-    //
+    //Tells to laravel what table it should use
+    protected $table = 'movies';
+
+    //Sets primary key
+    protected $primaryKey = 'movie_id';
+    public $incrementing = true;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'release_date',
+        'language',
+        'imdb_rating',
+        'poster_path',
+        'active'
+    ];
+
 }
