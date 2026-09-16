0934f29efca36c0c51c4f9f63d711122af79aac8
ThomasVerzijl <thomasverzijl27@gmail.com>
2026-09-16 16:13:17 +0200

    Add styling and layout to LoginPage

--- reactMoviePage/src/App.jsx
+++ reactMoviePage/src/App.jsx
@@ -1,17 +1,32 @@
 import LoginPage from "./components/jsx/Login.jsx";
 import Movie from "./components/jsx/Movie.jsx";
+import { useState, useEffect } from "react";
 
-import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
 const App = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [showLoginPage, setShowLoginPage] = useState(true);
+  const [showMoviePage, setShowMoviePage] = useState(false);
+
+  useEffect(() => {
+    checklogin();
+  });
+  const checklogin = () => {
+    if (isLoggedIn === true) {
+      setShowMoviePage(true);
+    } else {
+      setShowLoginPage(true);
+    }
+  };
   return (
     <>
       <h1>Anex Bios</h1>
-      <Movie />
+
+      {showLoginPage && <LoginPage />}
+      {showMoviePage && <Movie />}
     </>
   );
 };
-
 export default App;
