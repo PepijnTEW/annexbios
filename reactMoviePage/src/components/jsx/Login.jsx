f71c457676b0cd3f8a697fc387e71225fad4a3fa
ThomasVerzijl <thomasverzijl27@gmail.com>
2026-09-16 16:13:17 +0200

    Add styling and layout to LoginPage

--- reactMoviePage/src/components/jsx/Login.jsx
+++ reactMoviePage/src/components/jsx/Login.jsx
@@ -4,7 +4,18 @@ import "../css/Login.css";
 const LoginPage = () => {
   return (
     <div className="my-component">
-      <h1>My Component</h1>
+      <div className="loginPage">
+        <div className="loginContainer">
+          <h2 className="loginTitle">Login</h2>
+          <form className="loginForm" method="post">
+            <indent className="loginLabel">Gebruikers naam:</indent>
+            <input type="text" className="loginInput" />
+            <indent className="loginLabel">wachtwoord:</indent>
+            <input type="password" className="loginInput" />
+            <input type="submit" className="loginButton" value="Login" />
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
