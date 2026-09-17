d06456c2b20ae893d754f8f0bcefb90382c0f0af
ThomasVerzijl <thomasverzijl27@gmail.com>
2026-09-17 12:07:52 +0200

    added a hardcoded grid to show the movies and a checkbox to make active

--- reactMoviePage/src/App.jsx
+++ reactMoviePage/src/App.jsx
@@ -7,7 +7,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 
 const App = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState(true);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [showLoginPage, setShowLoginPage] = useState(false);
   const [showMoviePage, setShowMoviePage] = useState(false);
   const [moviesPlayingPage, setMoviesPlayingPage] = useState(true);
@@ -16,16 +16,15 @@ const App = () => {
     checklogin();
   });
   const checklogin = () => {
-    if (isLoggedIn === true) {
-      setShowMoviePage(true);
-    } else {
-      setShowLoginPage(true);
-    }
+    // if (isLoggedIn === true) {
+    //   setShowMoviePage(true);
+    // } else {
+    //   setShowLoginPage(true);
+    // }
   };
   return (
     <>
       <h1>Anex Bios</h1>
-
       {showLoginPage && <LoginPage />}
       {showMoviePage && <Movie />}
       {moviesPlayingPage && <MoviesPlaying />}
--- reactMoviePage/src/components/jsx/moviesPlaying.jsx
+++ reactMoviePage/src/components/jsx/moviesPlaying.jsx
@@ -1,6 +1,33 @@
 import { useState } from "react";
 import "../css/moviesplaying.css";
+import { Checkbox } from "@mui/material";
 const MoviesPlaying = () => {
-  return <h2>testttt</h2>;
+  return (
+    <>
+      <h2 id="movies-playing">Movies playing</h2>
+      <div className="app">
+        <div className="PMcontainer">
+          <div className="PMcard">
+            <div className="PMtitle">Movie Title</div>
+            <div className="PMid">Movie ID</div>
+            <p>enabled</p>
+          </div>
+          <div className="PMrowBorder"></div>
+          <div className="PMcard">
+            <div className="PMtitle">Fight Club</div>
+            <div className="PMid">101</div>
+            <Checkbox></Checkbox>
+          </div>
+          <div className="PMrowBorder"></div>
+          <div className="PMcard">
+            <div className="PMtitle">Hail Mary</div>
+            <div className="PMid">100</div>
+            <Checkbox></Checkbox>
+          </div>
+          <div className="PMrowBorder"></div>
+        </div>
+      </div>
+    </>
+  );
 };
 export default MoviesPlaying;
