aa1bab6db268618564bb36d04e343597c2dafad5
ThomasVerzijl <thomasverzijl27@gmail.com>
2026-09-17 10:28:24 +0200

    fixed typo

--- reactMoviePage/src/components/jsx/Login.jsx
+++ reactMoviePage/src/components/jsx/Login.jsx
@@ -6,10 +6,11 @@ const LoginPage = () => {
   const [logindata, setLoginData] = useState({});
   const handleSubmitLogin = (event) => {
     event.preventDefault();
-    const formData = new formData(event.target);
-    axios.post("http://example.com/api/endpoint", formData).then((response) => {
-      setLoginData(formData);
-    });
+    const formData = new FormData(event.target);
+    console.log("Ingevulde logindata:", Object.fromEntries(formData));
+    // axios.post("http://example.com/api/endpoint", formData).then((response) => {
+    //   setLoginData(formData);
+    // });
   };
   return (
     <div className="my-component">
