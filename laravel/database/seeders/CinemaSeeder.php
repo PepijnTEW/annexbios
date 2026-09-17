644a39e2268ddb62cfef7437f512030796dfc26b
Marylou Groeneveld <marylougroeneveld@gmail.com>
2026-09-17 09:59:32 +0200

    create seeders for users and cinema for premade accounts

--- laravel/database/seeders/CinemaSeeder.php
+++ laravel/database/seeders/CinemaSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use App\Models\Cinema;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class CinemaSeeder extends Seeder
 {
@@ -11,6 +13,29 @@ class CinemaSeeder extends Seeder
 
     public function run(): void
     {
-        Cinema::factory()->count(6)->create();
+        $cinemas = [
+            'Hoofdkantoor',
+            'Leerdam',
+            'Maarssen',
+            'Breukelen',
+            'Bilthoven',
+            'Montfoort',
+            'Woerden',
+            'Leidsche Rijn',
+            'Zeist'
+        ];
+
+        foreach ($cinemas as $city) {
+            $cinema = Cinema::create([
+                'cinema_name' => "Annex Bios $city"
+            ]);
+
+            User::create([
+                'name' => "Annex Bios $city",
+                'email' => strtolower($city) . '@annexbios.nl',
+                'password' => Hash::make('password'),
+                'cinema_id' => $cinema->cinema_id,
+            ]);
+        }
     }
 }
