67b2d116efed16d072c48a3b5b606538fa3b42c1
ThomasVerzijl <thomasverzijl27@gmail.com>
2026-09-21 09:00:18 +0200

    base for runTimePage

--- reactMoviePage/src/App.jsx
+++ reactMoviePage/src/App.jsx
@@ -1,6 +1,7 @@
 import LoginPage from "./components/jsx/Login.jsx";
 import Movie from "./components/jsx/Movie.jsx";
-import MoviesPlaying from "./components/jsx/moviesPlaying.jsx";
+import MoviesPlaying from "./components/jsx/MoviesPlaying.jsx";
+import RunTimes from "./components/jsx/RunTimes.jsx";
 import { useState, useEffect } from "react";
 
 import ReactDOM from "react-dom/client";
@@ -8,27 +9,29 @@ import "./index.css";
 
 const App = () => {
   // comment the checklogin function and put true in the usestate below to test certain pages
-  const [isLoggedIn, setIsLoggedIn] = useState(true);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [showLoginPage, setShowLoginPage] = useState(false);
   const [showMoviePage, setShowMoviePage] = useState(false);
   const [moviesPlayingPage, setMoviesPlayingPage] = useState(false);
+  const [runTimesPage, setrunTimesPage] = useState(true);
 
   useEffect(() => {
-    checklogin();
+    // checklogin();
   });
-  const checklogin = () => {
-    if (isLoggedIn === true) {
-      setShowMoviePage(true);
-    } else {
-      setShowLoginPage(true);
-    }
-  };
+  //   const checklogin = () => {
+  //     if (isLoggedIn === true) {
+  //       setShowMoviePage(true);
+  //     } else {
+  //       setShowLoginPage(true);
+  //     }
+  //   };
   return (
     <>
       <h1>Anex Bios</h1>
       {showLoginPage && <LoginPage />}
       {showMoviePage && <Movie />}
       {moviesPlayingPage && <MoviesPlaying />}
+      {runTimesPage && <RunTimes />}
     </>
   );
 };
---
+++ reactMoviePage/src/components/jsx/RunTimes.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../css/runTimes.css";
+
+const RunTimes = () => {
+  return (
+    <>
+      <h2>sjkdfnssssddskjf</h2>
+    </>
+  );
+};
+
+export default RunTimes;
