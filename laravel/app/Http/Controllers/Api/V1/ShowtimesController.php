bbf3894d3be2b6bc8f6a82fdc2ef4ea78d04dc1a
PepijnTEW <pepijntewinkel@gmail.com>
2026-09-24 11:59:08 +0200

    fixed wrong push

--- laravel/app/Http/Controllers/Api/V1/ShowtimesController.php
+++ laravel/app/Http/Controllers/Api/V1/ShowtimesController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Filters\V1\ShowtimesFilter;
-use App\Models\Showtimes;
+use App\Models\Showtime;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\V1\ShowtimesCollection;
 use App\Http\Resources\V1\ShowtimesResource;
