b691896949cf1fee1e6488c49a9b596b3a0ebc40
ThomasVerzijl <thomasverzijl27@gmail.com>
2026-09-25 11:39:29 +0200

    made an edit option

--- reactMoviePage/src/components/jsx/MovieCard.jsx
+++ reactMoviePage/src/components/jsx/MovieCard.jsx
@@ -1,6 +1,13 @@
 import React from "react";
+
 const MovieCard = ({ movie, onSaveMovie }) => {
-  const releaseYear = movie.release_date ? movie.release_date.slice(0, 4) : "Unknown";
+  const releaseYear = movie.release_date
+    ? movie.release_date.slice(0, 4)
+    : "Unknown";
+
+  const posterUrl = movie.poster_path
+    ? `https://image.tmdb.org/t/p/w500${movie.poster_path}`
+    : "https://placehold.co/400x600?text=No+Image";
 
   return (
     <div className="movie">
@@ -10,19 +17,25 @@ const MovieCard = ({ movie, onSaveMovie }) => {
 
       <div>
         <img
-          src={
-            movie.poster_path !== "null"
-              ? `https://image.tmdb.org/t/p/w500${movie.poster_path}`
-              : "https://placehold.co/400x600?text=No+Image"
-          }
+          src={posterUrl}
           alt={movie.title}
+          onError={(e) => {
+            e.target.onerror = null;
+            e.target.src = "https://placehold.co/400x600?text=No+Image";
+          }}
         />
       </div>
 
       <div>
-        <span>{movie.vote_average.toFixed(1)}</span>
+        <span>
+          {movie.vote_average ? movie.vote_average.toFixed(1) : "N/A"}
+        </span>
         <h3>{movie.title}</h3>
-        <button className="add-button" type="button" onClick={() => onSaveMovie(movie)}>
+        <button
+          className="add-button"
+          type="button"
+          onClick={() => onSaveMovie(movie)}
+        >
           Toevoegen
         </button>
       </div>
